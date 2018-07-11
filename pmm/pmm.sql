724c7409c95813b8c3a6bc07a9851e210cbe7778
mata-teh <35732465+mata-teh@users.noreply.github.com>
2018-07-11 16:43:48 +0700

    Update pmm.sql

--- pmm/pmm.sql
+++ pmm/pmm.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 06, 2018 at 05:53 PM
+-- Generation Time: Jul 11, 2018 at 11:42 AM
 -- Server version: 10.1.9-MariaDB
 -- PHP Version: 5.6.15
 
@@ -42,13 +42,13 @@ CREATE TABLE `images` (
 --
 
 INSERT INTO `images` (`id`, `file`, `author`, `category`, `caption`, `lists`, `description`, `date`) VALUES
-(9, 'uploads/bmp3_2k22.png', 'Guineapony', 'Russia and Former USSR', 'BMP-3 and Tunguska', 'Historical - Modern', 'Remastered', '2018-02-13 06:44:25'),
-(11, 'uploads/fv185.png', 'Blastwave', '1/72 Scale', 'FV-185 Centaur', 'Non-Historical - Science Fiction', 'Paragania (NationStates)', '2018-02-13 07:02:29'),
+(9, 'uploads/bmp3_2k22.png', 'Guineapony', 'Russia and Former USSR', 'BMP-3 and Tunguska', 'Historical_-_Modern', 'Remastered', '2018-02-13 06:44:25'),
+(11, 'uploads/fv185.png', 'Blastwave', '1/72 Scale', 'FV-185 Centaur', 'Non-Historical_-_Science_Fiction', 'Paragania (NationStates)', '2018-02-13 07:02:29'),
 (12, 'uploads/bears.png', 'Forrest Gump', 'Accessories', 'Bears', 'Masters', 'Black, Brown, Polar', '2018-07-04 05:48:00'),
-(13, 'uploads/laclass.png', 'InsanityBunni', 'Micro Scale', 'Los Angeles Class Nuclear Fast Attack Submarine', 'Historical - Cold War', 'Flight I, 1976', '2018-07-04 13:43:16'),
-(14, 'uploads/e13a.png', 'Neko', 'Japan', 'Aichi E13A &quot;Jake&quot;', 'Historical - WWII', 'Flying, On Water, Technicians', '2018-07-04 13:50:45'),
-(15, 'uploads/ALTPzKpfw_IV_mit_T-34-85_turm.png', 'TinkerTanker', '15mm Scale', 'PzKpfw IV mit T-34-85 turm', 'Non-Historical - Alternate History and Universe', 'Panzer IV with T-34/85 turret', '2018-07-06 15:33:20'),
-(16, 'uploads/MathiasComplete.png', 'Shermanus', '1/72 Scale', 'Mathias', 'Non-Historical - Fantasy', 'Tales of Redwall', '2018-07-06 15:47:59');
+(13, 'uploads/laclass.png', 'InsanityBunni', 'Micro Scale', 'Los Angeles Class Nuclear Fast Attack Submarine', 'Historical_-_Cold_War', 'Flight I, 1976', '2018-07-04 13:43:16'),
+(14, 'uploads/e13a.png', 'Neko', 'Japan', 'Aichi E13A &quot;Jake&quot;', 'Historical_-_WWII', 'Flying, On Water, Technicians', '2018-07-04 13:50:45'),
+(15, 'uploads/ALTPzKpfw_IV_mit_T-34-85_turm.png', 'TinkerTanker', '15mm Scale', 'PzKpfw IV mit T-34-85 turm', 'Non-Historical_-_Alternate_History_and_Universe', 'Panzer IV with T-34/85 turret', '2018-07-06 15:33:20'),
+(16, 'uploads/MathiasComplete.png', 'Shermanus', '1/72 Scale', 'Mathias', 'Non-Historical_-_Fantasy', 'Tales of Redwall', '2018-07-06 15:47:59');
 
 --
 -- Indexes for dumped tables
